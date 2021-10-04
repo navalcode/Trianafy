dc5ca78d161fb198816f5cab076b1c040ba4daa8
fran <gallego.gofra21@salesianos.triana.edu>
2021-10-04 17:22:21 +0200

    Metodo controller para listar todas las playlists

--- trianafy/src/main/java/controller/PlaylistController.java
+++ trianafy/src/main/java/controller/PlaylistController.java
@@ -1,4 +1,19 @@
 package controller;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import model.Playlist;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
 public class PlaylistController {
+    @GetMapping("/")
+    public ResponseEntity<List<Playlist>> findAll(){
+        return ResponseEntity.ok().body(repository.findAll());
+    }
 }
