e64670a7255393b7d232ea489abb553582d56e00
fran <gallego.gofra21@salesianos.triana.edu>
2021-10-04 17:24:51 +0200

    Método controller para obtener una playlist por su id

--- trianafy/src/main/java/controller/PlaylistController.java
+++ trianafy/src/main/java/controller/PlaylistController.java
@@ -12,8 +12,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 public class PlaylistController {
-    @GetMapping("/")
-    public ResponseEntity<List<Playlist>> findAll(){
-        return ResponseEntity.ok().body(repository.findAll());
+    @ApiResponse(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se ha encontrado la Playlist",
+                    content = { @Content(mediaType = "applicacion/json",
+                            schema = @Schema(implementation = Playlist.class))}),
+            @ApiResponse(responseCode = "400",
+                    description = "No se ha encontrado la Playlist por el ID",
+                    content = @Content),
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<Playlist> findOne(
+            @Parameter(description = "ID de la Playlist que desea buscar")
+            @PathVariable Long id
+    ){
+        return ResponseEntity.ok(repository.findById(id));
     }
 }
