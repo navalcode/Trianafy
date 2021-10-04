514e141ea80c93d7357201dc9527fb8998846aec
fran <gallego.gofra21@salesianos.triana.edu>
2021-10-04 17:29:41 +0200

    Metodo controller para agregar una Playlist

--- trianafy/src/main/java/controller/PlaylistController.java
+++ trianafy/src/main/java/controller/PlaylistController.java
@@ -5,27 +5,19 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import model.Playlist;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 public class PlaylistController {
-    @ApiResponse(value = {
-            @ApiResponse(responseCode = "200",
-                    description = "Se ha encontrado la Playlist",
-                    content = { @Content(mediaType = "applicacion/json",
-                            schema = @Schema(implementation = Playlist.class))}),
-            @ApiResponse(responseCode = "400",
-                    description = "No se ha encontrado la Playlist por el ID",
-                    content = @Content),
-    })
-    @GetMapping("/{id}")
-    public ResponseEntity<Playlist> findOne(
-            @Parameter(description = "ID de la Playlist que desea buscar")
-            @PathVariable Long id
-    ){
-        return ResponseEntity.ok(repository.findById(id));
+
+    @PostMapping("/")
+    public ResponseEntity<Playlist> create(@RequestBody Playlist nueva){
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(nueva));
     }
 }
