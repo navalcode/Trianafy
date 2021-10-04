ac45b5bf91355298fb853b2790672a97d6cf2535
fran <gallego.gofra21@salesianos.triana.edu>
2021-10-04 17:18:57 +0200

    Creando entidad, repositorio y controller de Playlist

--- trianafy/src/main/java/controller/PlaylistController.java
+++ trianafy/src/main/java/controller/PlaylistController.java
@@ -1,4 +1,29 @@
 package controller;
 
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import model.Playlist;
+import org.apache.coyote.Response;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import repository.PlaylistRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/playlist")
+@RequiredArgsConstructor
 public class PlaylistController {
+
+    private final PlaylistRepository repository;
+
+
 }
