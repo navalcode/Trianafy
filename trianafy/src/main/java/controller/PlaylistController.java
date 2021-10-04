5d7d17fbf8abb1dac3a264d4dc690f90d411a2f8
fran <gallego.gofra21@salesianos.triana.edu>
2021-10-04 17:35:15 +0200

    Método controller para obtener una Playlist por su id y modificar su información

--- trianafy/src/main/java/controller/PlaylistController.java
+++ trianafy/src/main/java/controller/PlaylistController.java
@@ -7,17 +7,24 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import model.Playlist;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 public class PlaylistController {
 
-    @PostMapping("/")
-    public ResponseEntity<Playlist> create(@RequestBody Playlist nueva){
-        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(nueva));
+    @PutMapping("/{id}")
+    public ResponseEntity<Playlist> edit(
+            @RequestBody Playlist p,
+            @PathVariable Long id){
+        return ResponseEntity.off(
+                repository.findById(id).map(c -> {
+                    c.setNombre(p.getNombre()),
+                    c.setDescripcion(p.getDescripcion());
+                    repository.save(c);
+                    return c;
+                })
+        );
     }
+
 }
