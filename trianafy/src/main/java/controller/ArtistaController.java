7126271716c12b36102094a4de53e358eb194f3d
Manuel Naval <navalrod@gmail.com>
2021-10-05 10:24:00 +0200

    Método para borrar un artista por su id

--- trianafy/src/main/java/controller/ArtistaController.java
+++ trianafy/src/main/java/controller/ArtistaController.java
@@ -1,7 +1,13 @@
 package controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import repository.ArtistaRepository;
@@ -9,8 +15,19 @@ import repository.ArtistaRepository;
 @RestController
 @RequestMapping("/artist")
 @RequiredArgsConstructor
-@Tag(name="Artista", description = "Esta clase implementa controladores REST para Artista")
+@Tag(name = "Artista", description = "Esta clase implementa controladores REST para Artista")
 public class ArtistaController {
 
     private final ArtistaRepository artistaRepository;
+
+    @Operation(summary = "Elimina un artista por su id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",
+                    description = "Artista Eliminado correctamente")})
+
+    @DeleteMapping("/id")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        artistaRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
