ac7a7860013317cc29103b93d89563af0cea7dd2
Francisco Gallego Gordillo <gallego.gofra21@triana.salesianos.edu>
2021-10-05 10:35:02 +0200

    Añadiendo anotaciones y repositorio

--- trianafy/src/main/java/controller/PlaylistController.java
+++ trianafy/src/main/java/controller/PlaylistController.java
@@ -4,14 +4,24 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.RequiredArgsConstructor;
 import model.Playlist;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import repository.PlaylistRepository;
 
 import java.util.List;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/playlist")
 public class PlaylistController {
+
+    private final PlaylistRepository repository;
+
     @GetMapping("/")
     public ResponseEntity<List<Playlist>> findAll(){
         return ResponseEntity.ok().body(repository.findAll());
