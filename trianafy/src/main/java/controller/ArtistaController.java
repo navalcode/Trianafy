fc96e0a90a0365f0991bcf7d6170845c836ae562
Manuel Naval <navalrod@gmail.com>
2021-10-05 10:03:55 +0200

    Método para buscar un artista por su id

--- trianafy/src/main/java/controller/ArtistaController.java
+++ trianafy/src/main/java/controller/ArtistaController.java
@@ -1,16 +1,46 @@
 package controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import model.Artista;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import repository.ArtistaRepository;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/artist")
 @RequiredArgsConstructor
-@Tag(name="Artista", description = "Esta clase implementa controladores REST para Artista")
+@Tag(name = "Artista", description = "Esta clase implementa controladores REST para Artista")
 public class ArtistaController {
 
     private final ArtistaRepository artistaRepository;
+
+    @Operation(summary = "Obtiene un artista por su id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se ha encontrado el artista",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Artista.class))}),
+            @ApiResponse(responseCode = "404",
+                    description = "No se han encontrado el artista",
+                    content = @Content),
+    })
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Artista> findOne(@Parameter("El id del artista a buscar")
+                                                     @PathVariable Long id) {
+        return ResponseEntity.of(artistaRepository.findById(id));
+
+    }
 }
