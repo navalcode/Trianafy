3647159f2618b0e8a2b9f792af43b752090b4a72
Manuel Naval <navalrod@gmail.com>
2021-10-05 09:50:22 +0200

    Método para crear un artista

--- trianafy/src/main/java/controller/ArtistaController.java
+++ trianafy/src/main/java/controller/ArtistaController.java
@@ -8,10 +8,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import model.Artista;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import repository.ArtistaRepository;
 
 import java.util.List;
@@ -19,16 +18,33 @@ import java.util.List;
 @RestController
 @RequestMapping("/artist")
 @RequiredArgsConstructor
-@Tag(name="Artista", description = "Esta clase implementa controladores REST para Artista")
+@Tag(name = "Artista", description = "Esta clase implementa controladores REST para Artista")
 public class ArtistaController {
 
     private final ArtistaRepository artistaRepository;
+
+    @Operation(summary = "Crea un artista con el modelo obtenido del cuerpo de la petición.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Se ha creado el artista",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Artista.class))}),
+            @ApiResponse(responseCode = "400",
+                    description = "No se ha podido crear el artista, petición erronea.",
+                    content = @Content),
+    })
+    @PostMapping
+    public ResponseEntity<Artista> crate(@RequestBody Artista nuevoArtista) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(artistaRepository.save(nuevoArtista));
+    }
 
     @Operation(summary = "Obtiene todos los artistas existentes en el repositorio")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
                     description = "Se han encontrado los artistas",
-                    content = { @Content(mediaType = "application/json",
+                    content = {@Content(mediaType = "application/json",
                             schema = @Schema(implementation = Artista.class))}),
             @ApiResponse(responseCode = "404",
                     description = "No se han encontrado los artistas",
@@ -36,7 +52,7 @@ public class ArtistaController {
     })
 
     @GetMapping("/")
-    public ResponseEntity<List<Artista>> findAll(){
+    public ResponseEntity<List<Artista>> findAll() {
         return ResponseEntity
                 .ok()
                 .body(artistaRepository.findAll());
