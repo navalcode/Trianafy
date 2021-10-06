d17522583e68ba447151a3638a72d45671d1e1dd
arramos270 <alexrramos270@gmail.com>
2021-10-06 19:23:57 +0200

    Terminado el eliminar una canción por su id

--- trianafy/src/main/java/com/salesianostriana/dam/trianafy/controller/SongController.java
+++ trianafy/src/main/java/com/salesianostriana/dam/trianafy/controller/SongController.java
@@ -53,5 +53,9 @@ public class SongController {
                 })
         );
     }
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
