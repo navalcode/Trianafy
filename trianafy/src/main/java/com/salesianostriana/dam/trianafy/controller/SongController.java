6eb6702668eb3ea2b2639cfed866637aef18d9bf
Manuel Naval <navalrod@gmail.com>
2021-10-07 17:37:26 +0200

    minor bug fixed

--- trianafy/src/main/java/com/salesianostriana/dam/trianafy/controller/SongController.java
+++ trianafy/src/main/java/com/salesianostriana/dam/trianafy/controller/SongController.java
@@ -13,6 +13,7 @@ import com.salesianostriana.dam.trianafy.repository.SongRepository;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
